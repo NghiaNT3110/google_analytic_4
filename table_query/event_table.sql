71d8b9338cbe481a2dfd741998aeafd4574a555b
Nguyen Trung Nghia <156154939+NghiaNT3110@users.noreply.github.com>
2024-06-10 15:25:43 +0700

    Update event_table.sql with conversion marker

--- table_query/event_table.sql
+++ table_query/event_table.sql
@@ -8,6 +8,17 @@ SELECT
   device.web_info.browser AS browser,
   geo.country, geo.city,
   event_name,
+  CASE WHEN 
+  event_name LIKE '%view_item'
+  OR event_name LIKE '%add_to_cart%'
+  OR event_name LIKE '%begin_check%'
+  OR event_name LIKE '%select_item%'
+  OR event_name LIKE '%add%'
+  OR event_name LIKE '%select_promo%'
+  OR event_name LIKE '%purchase%'
+  THEN 'TRUE'
+  ELSE 'FALSE' 
+  END AS is_conversion,
   FROM
   `big-query-378507.analytics_402516150.events_*`
   ORDER BY event_date DESC
